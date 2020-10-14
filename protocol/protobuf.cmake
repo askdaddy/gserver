5a8dd9bda0f753fb00fe2da0d86fd1f807867247
Seven <humen1@gmail.com>
2020-10-14 11:26:41 +0800

    把protocol独立出来

--- protocol/protobuf.cmake
+++ protocol/protobuf.cmake
@@ -1,9 +1,12 @@
 include( FindProtobuf )
 message("include Protobuf")
+message(${CMAKE_CURRENT_BINARY_DIR})
 
 find_package(Protobuf REQUIRED)
 include_directories(${Protobuf_INCLUDE_DIRS})
-include_directories(${CMAKE_CURRENT_BINARY_DIR})
+#include_directories(${CMAKE_CURRENT_BINARY_DIR})
+set(PROTO_BIN_DIR ${CMAKE_CURRENT_BINARY_DIR})
+set(PROTOCOL_HEADERS ${CMAKE_CURRENT_BINARY_DIR} ${Protobuf_INCLUDE_DIRS})
 
 file(GLOB PROTO_LIST ${CMAKE_CURRENT_SOURCE_DIR}/*.proto)
 protobuf_generate_cpp(PROTO_SRC PROTO_HEADER ${PROTO_LIST})
