4aa746313bad0bdf99d528078c4d39286e4e6a4d
Seven <humen1@gmail.com>
2020-10-14 10:49:46 +0800

    add client

---
+++ client.js/index.js
@@ -0,0 +1,43 @@
+var net = require('net');
+var protobuf = require("protobufjs");
+
+var HOST = '127.0.0.1';
+var PORT = 12400;
+
+var client = new net.Socket();
+client.connect(PORT, HOST, function () {
+    console.log('CONNECTED TO: ' + HOST + ':' + PORT);
+    // client.write(buff);
+    run();
+});
+
+client.on('data', function (data) {
+    console.log('Data: ' + data);
+    // client.destroy();
+});
+
+client.on('close', function () {
+    console.log('Connection closed');
+});
+
+
+function run() {
+    protobuf.load("msg.proto", function (err, root) {
+        if (err) throw err;
+        var telegram = root.lookupType("labs_proto.telegram");
+        var payload = { code: 0, msg: " - hello world! | the best world! <" };
+
+        // var errMsg = telegram.verify(payload);
+        // if (errMsg) throw Error(errMsg);
+
+        for (var i=0; i < 300; ++i) {
+            payload.code = i;
+            console.log(payload);
+            var message = telegram.create(payload);
+            var b = telegram.encode(message).finish();
+            client.write(b);
+        }
+        // client.destroy();
+    });
+}
+
