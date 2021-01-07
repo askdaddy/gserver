b9cde2c9ed269f535b9b8998fa0f3a1e062bf895
Seven <seven@apowo.com>
2021-01-07 10:50:21 +0800

    test

--- main.cpp
+++ main.cpp
@@ -1,48 +1,6 @@
-//#include <test_buffer.h>
-//#include "reflection/factory.h"
-//#include "reflection/classes.h"
 #include<iostream>
 
-//class A {
-//public:
-//    A() = default;
-//
-//    virtual ~A() = default;
-//
-//    void foo(int i) {
-//        std::cout << "A" << std::endl;
-//    }
-//
-//private:
-//    int a;
-//};
-//
-//class AA : public A {
-//public:
-//    AA() = default;
-//
-//    virtual ~AA() = default;
-//
-//    void foo(int i) {
-//        std::cout << "AA" << std::endl;
-//    }
-//
-//    void fn() {
-//        std::cout << "AA::fn" << std::endl;
-//    }
-//};
-
-
-//void func(A &a) {
-//    a.foo(0);
-//    AA aa = dynamic_cast<AA&>(a);
-//    aa.fn();
-//
-//}
-
-
-//typedef void (*static_fn)();
-
+using namespace std;
 
 class P {
 public:
@@ -50,16 +8,12 @@ public:
 
     virtual ~P() = default;
 
-    void foo() {
-        std::cout << "This is P::foo()" << std::endl;
-    }
-
-    void bar() {
-        std::cout << "This is P::bar()" << std::endl;
-    }
-
     bool operator==(P p) const {
         return p.getId() == m_id;
+    }
+
+    bool operator!=(P p) const {
+        return p.getId() != m_id;
     }
 
     int getId() { return m_id; }
@@ -74,14 +28,9 @@ typedef void (P::*p_function)();
 int main() {
     P p1(1), p2(1), p3(2);
 
-//    p_function foo_ptr = &P::foo;
-//    p_function bar_ptr = &P::bar;
-    // Call
-//    (pp.*foo_ptr)();
-//    (pp.*bar_ptr)();
-
-    p1 == p2;
-    p1 == p3;
+    cout << (p1 == p2) << endl;
+    cout << (p1 != p3) << endl;
+    cout << (p1 == p3) << endl;
 
     return 0;
 }
